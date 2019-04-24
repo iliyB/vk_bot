78369e0b0fedf4d1a671cd6b1f0b6a4a18013275
Iliy <iliy-b@mail.ru>
2019-04-24 11:05:39 +0300

    2

--- bot.py
+++ bot.py
@@ -1,48 +1,67 @@
 import bs4 as bs4
-import request
+import requests
+
 
 class VkBot:
 
-	def __init__(self,user_id):
-		print('Создан объект бота!!')
+    def __init__(self, user_id):
+        print("Создан объект бота!")
 
-		self._USER_ID=user_id
-		self._USERNAME_=self._get_user_name_from_vk_id(user_id)
+        self._USER_ID = user_id
+        self._USERNAME = self._get_user_name_from_vk_id(user_id)
 
-		self._COMMANDS=["ПРИВЕТ","ПОГОДА",'ВРЕМЯ','ПОКА']
+        self._COMMANDS = ["ПРИВЕТ", "ПОГОДА", "ВРЕМЯ", "ПОКА","ХОЧУ ДОМОЙ","ЕДА","КОМАНДЫ"]
 
-	
-	def _get_user_name_from_vk_id(self, user_id):
+    def _get_user_name_from_vk_id(self, user_id):
         request = requests.get("https://vk.com/id"+str(user_id))
         bs = bs4.BeautifulSoup(request.text, "html.parser")
 
         user_name = self._clean_all_tag_from_str(bs.findAll("title")[0])
 
         return user_name.split()[0]
-    
 
-    def new_message(self,message):
 
-    	#Привет
-    	if message.upper()=self._COMMANDS[0]:
-    		return f"Привет, {self._USERNAME_}!"
 
-    	#Погода
-    	elif message.upper()=self._COMMANDS[1]:
-    		return self._get_weather()
+    def all_commands(self):
+    	s=""
+    	for item in self._COMMANDS:
+    		s+=item+", "
+    	return s
 
-    	#Время
-    	elif message.upper()=self._COMMANDS[2]:
-    		return self._get_time()
+    def new_message(self, message):
 
-    	#Пока
-    	elif message.upper()=self._COMMANDS[3]:
-    		return f"Пока, {self._USERNAME_}!"
+        # Привет
+        if message.upper() == self._COMMANDS[0]:
+            return f"Привет, {self._USERNAME}"
+        # Погода
+        elif message.upper() == self._COMMANDS[1]:
+            return self._get_weather()
 
-    	else:
-    		return "Я не понимаю, что вы написали...."
+        # Время
+        elif message.upper() == self._COMMANDS[2]:
+            return self._get_time()
 
-     @staticmethod
+        # Пока
+        elif message.upper() == self._COMMANDS[3]:
+            return f"Пока, {self._USERNAME}!"
+
+        elif message.upper() == self._COMMANDS[4]:
+            return "Приезжай"
+
+        elif message.upper() == self._COMMANDS[5]:
+            return "Покушай"
+
+        elif message.upper() == self._COMMANDS[6]:
+            return self.all_commands()
+        else:
+            return "Не понимаю о чем вы..."
+
+    def _get_time(self):
+        request = requests.get("https://my-calend.ru/date-and-time-today")
+        b = bs4.BeautifulSoup(request.text, "html.parser")
+        return self._clean_all_tag_from_str(str(b.select(".page")[0].findAll("h2")[1])).split()[1]
+
+    @staticmethod
     def _clean_all_tag_from_str(string_line):
 
         """
@@ -66,7 +85,7 @@ class VkBot:
         return result
 
     @staticmethod
-    def _get_weather(city: str = "санкт-петербург") -> list:
+    def _get_weather(city: str = "владимир") -> list:
 
         request = requests.get("https://sinoptik.com.ru/погода-" + city)
         b = bs4.BeautifulSoup(request.text, "html.parser")
@@ -88,9 +107,3 @@ class VkBot:
         result = result + weather.strip()
 
         return result
-
-
-
-
-
-
--- main.py
+++ main.py
@@ -2,14 +2,24 @@ import vk_api
 from vk_api.longpoll import VkLongPoll,VkEventType
 
 from bot import VkBot
-
+import datetime
+now=datetime.datetime.now()
 
 token="2e206344c6f275c520ace5c8f856f61b4dc8f1833142dc767a7c6a96a42e44c4c252213434609c431cf13" 
 N_message=1
 
 def write_message(user_id,message):
-	vk.method('messge.send',{'user_id':user_id,'message':message,'random_id':N_message})
-	N_message+=1
+	global N_message
+	tim=now.hour*100 + now.minute+now.second
+	if message=="Приезжай":
+		vk.method('messages.send', {'user_id': user_id, 'message': message,'random_id': tim+N_message})
+		N_message+=1
+		vk.method('messages.send', {'user_id': user_id, 'message': message,'random_id':tim+N_message,'sticker_id':53})
+		N_message+=1
+	else:
+		vk.method('messages.send', {'user_id': user_id, 'message': message,'random_id':tim+N_message})
+		N_message+=1
+	
 
 vk=vk_api.VkApi(token=token)
 
@@ -17,7 +27,7 @@ longpoll=VkLongPoll(vk)
 
 print("Server started")
 for event in longpoll.listen():
-	if event.to_me and event.type=VkEventType.MESSAGE_NEW:
+	if event.to_me and event.type==VkEventType.MESSAGE_NEW:
 		print('New message')
 		print(f'For me by: {event.user_id}', end='')
 
